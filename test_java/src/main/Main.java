923441a58061bfcc549f1dfddcfba2f1150962cd
josedonoso <j.donoso.catalan@gmail.com>
2017-11-02 10:01:58 -0300

    Se ha creado la clase cliente y una instancia en main

--- test_java/src/main/Main.java
+++ test_java/src/main/Main.java
@@ -8,6 +8,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo desde Java!");
+        
+        Cliente c = new Cliente(1, "Nombre");
+        
+        System.out.println(c.getNombre());
     }
 
 }
